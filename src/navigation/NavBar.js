6f356bbed5a7d973bc56547778c7298be8e82e07
Tan <tan10tvo@gmail.com>
2022-01-27 15:53:53 -0700

    add images-nav-home-about

---
+++ src/navigation/NavBar.js
@@ -0,0 +1,18 @@
+
+function NavBar(){
+
+    return(
+        <nav>
+            <ul>
+                <li>
+                    Home
+                    About
+                    People
+                    Add person
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
