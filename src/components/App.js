7dcb7ffde4bd25d368689eeb2d602595c02f57c3
Tan <tan10tvo@gmail.com>
2022-01-28 20:23:46 -0700

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import Home from './Home';
 import NavBar from '../navigation/NavBar';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from '../navigation/Header'
 
 
 function App() {
   return (
     <div className="App">
+      
+      <Router>
+    <NavBar />
+    <Header />
+    <Switch>
+    <Route path="/">
       <Home />
-     <NavBar />
-
+    </Route>
+      </Switch>
+      </Router>
+    
+    <Footer />
     </div>
   );
 }
